ce4d0b1e069c090f7fbf093d23a769f2ce4dab5a
salihulukoylu <dxlearn@gmail.com>
2024-12-23 01:49:32 +0300

    commit
    commit

--- EpeyScrapper/EpeyScrapper/Program.cs
+++ EpeyScrapper/EpeyScrapper/Program.cs
@@ -11,14 +11,17 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        string cookie = "_gid=GA1.2.791491693.1734544251; PHPSESSID=537801c74fba6029c37ee29a2ed9e583; cf_clearance=hmxSmA7JFCBqQ3ihqdLzmKQ0CaC_J1.96pxvQROXNd4-1734545542-1.2.1.1-mo77epaCAnPfU3qPe.eVBJuHi2H_uV_uNrvbzZn6AYND8B0DYPKMesHfsxnLjzwjA3MX7t3X0j.kRNpY4QjZoDtLG8940nuhreYejWRdbj3goCYF0M7RRRmxBRGe5MRtmT.YCzQkO3wMX_9xJ0MqaRi87dRCIkvZspQicXNwxqtLAWVg7ClZ7xQC7VuPWH3WI8sAUlG1BuHSGfVL8SiwbRZSWFqSLJV2vPW06KKjRZ75MehjeQ6UayaWJVcSM8ye5d2T40hhnBpagNH5GK6HkD.deMDZT_INFz0IXXDqr.fK8627IN5CvmPiZAxMY7wG5BzVUAi.d8e0gt.SY5m86dnMINsk5EWH1HxK9cFgIdVob1nUOOCOzgF4othOiIDfvH0lrjLud6uHUeW9rbULBtAvhvmJKW_z3dROubWxBHgJxnX7OPXTmkZuXUWQ52gW";
-        int totalPages = 72; // Kaç sayfa scrape edilecek
-        string outputFile = "phone_links.txt";
+     
+        string cookie = "_gid=GA1.2.1893669592.1734558928; renkler=; PHPSESSID=ece4f8a58fd6ad0060caacd9555ce68d; _gat=1; cf_clearance=1ClbLpfcZmsqzS1EfQYwHwpZMUqGlFFW8GJoEDwWki8-1734656408-1.2.1.1-qxAll5i1Mlx_LCQzWImUIP8WnD20E.cMIOQ_pkI.1LlO1aYky758iE7fWmYP9bQlytINzpJkTY36pfQwgXQyJhoRJIFemafx.7B6GGWYZOrdQ6ujC1G37n3wxMbYW3Ree4FGEvKI9nhgLW0iigNLHzLW34taAmdXz7a70Fvi1zjnrkWzb1Ex1nZ0wAD48955aTiLwr9iE3QAcBjoN4uH1LM97LNr99usWm.fYymloW8tJ07_foACAqHgMFdrMzjkOWSFCNm_oHxBuREk8sW7iZ0V9lHAvWI105MT_8anb502GeNUQ2_dyUnVP1s_yyQC0IY5K2HdAYaSItBsw9WkZ4iBxk2H_95V8rh.jsdC_c0CwhkYOng3fl8nlhk4blyM7sf3ICnHU8qS_m5cGFZ9JrfNtnemIxh2yszs5o46Cg0B1QJLDxj4h14AFgkmwJ9j; _ga=GA1.1.1930074104.1734558928; __gads=ID=9f9ebf79f1523849:T=1734558928:RT=1734656408:S=ALNI_Mbp8CnT5pB-jENthJuVcUpOTSmbsw; __gpi=UID=00000f71be9c09a2:T=1734558928:RT=1734656408:S=ALNI_MY65zEFwIY9af7Bb10erBFyZAphdg; __eoi=ID=2713b89900ec94e6:T=1734558928:RT=1734656408:S=AA-AfjaxmpmereZqgCkkOWwkTNKQ; _ga_MZXRL7TKL9=GS1.1.1734656403.11.1.1734656409.0.0.0";
 
+        int totalPages = 72; // Toplam taranacak sayfa sayısı
+        string outputFile = "phone_links.txt"; // Kaydedilecek linklerin dosya adı
+
+        // Link toplama işlemi
         //Console.WriteLine("Link toplama işlemi başlıyor...");
         //var links = await GetLinkleriTopla(cookie, totalPages);
 
-        //// Linkleri kaydet
+        //// Linkleri dosyaya kaydet
         //if (links.Count > 0)
         //{
         //    File.WriteAllLines(outputFile, links);
@@ -29,101 +32,12 @@ class Program
         //    Console.WriteLine("Hiç link bulunamadı.");
         //}
 
+        // Kaydedilen linkleri gezme işlemi
         Console.WriteLine("Kaydedilen linkler geziliyor...");
         await GetLinkleriGez(cookie, outputFile);
 
         Console.WriteLine("Linkleri gezme işlemi tamamlandı.");
     }
-
-    static async Task GetLinkleriGez(string cookie, string inputFile)
-    {
-        if (!File.Exists(inputFile))
-        {
-            Console.WriteLine($"Hata: {inputFile} dosyası bulunamadı.");
-            return;
-        }
-
-        var links = File.ReadAllLines(inputFile);
-        if (links.Length == 0)
-        {
-            Console.WriteLine("Hata: Dosyada link bulunamadı.");
-            return;
-        }
-
-        using (HttpClientHandler handler = new HttpClientHandler())
-        {
-            handler.AutomaticDecompression = System.Net.DecompressionMethods.GZip | System.Net.DecompressionMethods.Deflate;
-
-            using (HttpClient client = new HttpClient(handler))
-            {
-                client.DefaultRequestHeaders.Add("Cookie", cookie);
-                client.DefaultRequestHeaders.Add("Accept", "text/html, */*; q=0.01");
-                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.86 Safari/537.36");
-
-                foreach (var link in links)
-                {
-                    Console.WriteLine($"Taranıyor: {link}");
-                    try
-                    {
-                        HttpResponseMessage response = await client.GetAsync(link);
-                        if (response.IsSuccessStatusCode)
-                        {
-                            string html = await response.Content.ReadAsStringAsync();
-
-                            // HTML içeriğini parse et
-                            HtmlDocument document = new HtmlDocument();
-                            document.LoadHtml(html);
-
-                            // Marka adını al
-                            var brandNode = document.DocumentNode.SelectSingleNode("//div[@class='yol']/a[last()]");
-                            string brandName = brandNode != null ? brandNode.InnerText.Trim() : "Bilinmeyen Marka";
-
-                            // Ürün ismini al
-                            var productNode = document.DocumentNode.SelectSingleNode("//div[@class='baslik']/h1/a");
-                            string productModel = productNode != null
-                                ? productNode.GetAttributeValue("title", "Bilinmeyen Ürün").Trim()
-                                : "Bilinmeyen Ürün";
-
-                            // Proje dizininde markaya ait klasör kontrolü
-                            string projectDirectory = Directory.GetCurrentDirectory() + "/uploads";
-                            string brandDirectory = Path.Combine(projectDirectory, brandName);
-                            string productDirectory = Path.Combine(brandDirectory, productModel);
-
-                            // Marka klasörünü oluştur
-                            if (!Directory.Exists(brandDirectory))
-                            {
-                                Directory.CreateDirectory(brandDirectory);
-                                Console.WriteLine($"Marka klasörü oluşturuldu: {brandDirectory}");
-                            }
-
-                            // Model klasörünü oluştur
-                            if (!Directory.Exists(productDirectory))
-                            {
-                                Directory.CreateDirectory(productDirectory);
-                                Console.WriteLine($"Model klasörü oluşturuldu: {productDirectory}");
-                            }
-                            else
-                            {
-                                Console.WriteLine($"Model klasörü zaten mevcut: {productDirectory}");
-                            }
-
-                            Console.WriteLine($"Ürün: {productModel}");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Hata: {response.StatusCode} - {link}");
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"Hata: {ex.Message} - {link}");
-                    }
-                }
-            }
-        }
-    }
-
-
 
     static async Task<List<string>> GetLinkleriTopla(string cookie, int totalPages)
     {
@@ -164,7 +78,35 @@ class Program
                             // HTML Parsing
                             HtmlDocument document = new HtmlDocument();
                             document.LoadHtml(html);
+                            // Marka adını al
+                            var brandNode = document.DocumentNode.SelectSingleNode("//div[@class='yol']/a[last()]");
+                            string brandName = brandNode != null ? brandNode.InnerText.Trim() : "Bilinmeyen Marka";
 
+                            // Ürün ismini al
+                            var productNode = document.DocumentNode.SelectSingleNode("//div[@class='baslik']/h1/a");
+                            string productModel = productNode != null
+                                ? productNode.GetAttributeValue("title", "Bilinmeyen Ürün").Trim()
+                                : "Bilinmeyen Ürün";
+
+                            // Proje dizininde markaya ait klasör kontrolü
+                            string projectDirectory = Directory.GetCurrentDirectory() + "/uploads";
+                            string brandDirectory = Path.Combine(projectDirectory, brandName);
+                            string productDirectory = Path.Combine(brandDirectory, productModel);
+
+                            // Marka klasörünü oluştur
+                            if (!Directory.Exists(brandDirectory))
+                            {
+                                Directory.CreateDirectory(brandDirectory);
+                                Console.WriteLine($"Marka klasörü oluşturuldu: {brandDirectory}");
+                            }
+
+                            // Model klasörünü oluştur
+                            if (!Directory.Exists(productDirectory))
+                            {
+                                Directory.CreateDirectory(productDirectory);
+                                Console.WriteLine($"Model klasörü oluşturuldu: {productDirectory}");
+                            }
+                        
                             // Telefon linklerini bul
                             var links = document.DocumentNode.SelectNodes("//div[@class='detay cell']//a[@class='urunadi']")
                                 ?.Select(node => node.Attributes["href"].Value)
@@ -192,4 +134,186 @@ class Program
             }
         }
     }
+
+    static async Task GetLinkleriGez(string cookie, string inputFile)
+    {
+        if (!File.Exists(inputFile))
+        {
+            Console.WriteLine($"Hata: {inputFile} dosyası bulunamadı.");
+            return;
+        }
+
+        var links = File.ReadAllLines(inputFile);
+        if (links.Length == 0)
+        {
+            Console.WriteLine("Hata: Dosyada link bulunamadı.");
+            return;
+        }
+
+        string logFilePath = "downloaded_images.txt";
+
+        HashSet<string> processedLinks = new HashSet<string>();
+        if (File.Exists(logFilePath))
+        {
+            var loggedUrls = File.ReadAllLines(logFilePath);
+            processedLinks = new HashSet<string>(loggedUrls);
+            Console.WriteLine($"Toplam {processedLinks.Count} link logdan yüklendi.");
+        }
+        else
+        {
+            Console.WriteLine("Log dosyası bulunamadı, yeni oluşturulacak.");
+        }
+
+
+        using (HttpClientHandler handler = new HttpClientHandler())
+        {
+            handler.AutomaticDecompression = System.Net.DecompressionMethods.GZip | System.Net.DecompressionMethods.Deflate;
+
+            using (HttpClient client = new HttpClient(handler))
+            {
+                client.DefaultRequestHeaders.Add("Cookie", cookie);
+                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.86 Safari/537.36");
+
+                foreach (var link in links.OrderBy(o=> o.Length))
+                {
+                    //if (processedLinks.Contains(link))
+                    //{
+                    //    Console.WriteLine($"Link zaten işlendi: {link}");
+                    //    continue; // İşlenmiş linki atla
+                    //}
+
+                    if(link.Contains("infinix"))
+                    {
+                        Console.WriteLine($"Taranıyor: {link}");
+
+                        // İşlenen linki loga kaydet
+                        processedLinks.Add(link);
+                        await File.AppendAllTextAsync(logFilePath, link + Environment.NewLine);
+
+                        try
+                        {
+                            HttpResponseMessage response = await client.GetAsync(link);
+                            if (response.IsSuccessStatusCode)
+                            {
+                                string html = await response.Content.ReadAsStringAsync();
+
+                                // HTML içeriğini parse et
+                                HtmlDocument document = new HtmlDocument();
+                                document.LoadHtml(html);
+
+                                // Marka ve model bilgilerini al
+                                var brandNode = document.DocumentNode.SelectSingleNode("//div[@class='yol']/a[last()]");
+                                string brandName = brandNode != null ? brandNode.InnerText.Trim() : "Bilinmeyen Marka";
+
+                                var productNode = document.DocumentNode.SelectSingleNode("//div[@class='baslik']/h1/a");
+                                string productModel = productNode != null
+                                    ? productNode.GetAttributeValue("title", "Bilinmeyen Ürün").Trim()
+                                    : "Bilinmeyen Ürün";
+
+                                // Klasör oluşturma
+                                string projectDirectory = Directory.GetCurrentDirectory() + "/uploads";
+                                string brandDirectory = Path.Combine(projectDirectory, brandName);
+                                string productDirectory = Path.Combine(brandDirectory, productModel);
+
+                                if (!Directory.Exists(productDirectory))
+                                    Directory.CreateDirectory(productDirectory);
+
+                                Console.WriteLine($"Ürün: {productModel}");
+
+                                // Görsel URL'leri al
+                                var imgNodes = document.DocumentNode.SelectNodes("//div[@class='buyuk row']//img");
+                                if (imgNodes != null)
+                                {
+                                    foreach (var imgNode in imgNodes)
+                                    {
+                                        string imgUrl = imgNode.GetAttributeValue("src", "");
+                                        if (!string.IsNullOrEmpty(imgUrl))
+                                        {
+                                            // Ürün resimlerinin URL'lerini oluştur
+                                            List<string> imageUrls = GenerateImageUrls(imgUrl, 35);
+                                            foreach (var imageUrl in imageUrls)
+                                            {
+                                                Console.WriteLine($"Resim sorgulanıyor: {imageUrl}");
+                                                await DownloadAndSaveImage(client, imageUrl, productDirectory, logFilePath);
+                                            }
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Resim bulunamadı.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Hata: {response.StatusCode} - {link}");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Hata: {ex.Message} - {link}");
+                        }
+                    }
+
+                  
+
+                 
+                }
+            }
+        }
+    }
+
+    static List<string> GenerateImageUrls(string baseUrl, int maxImages)
+    {
+        List<string> urls = new List<string>();
+
+        // s_ veya m_ varsa b_ yap
+        if (baseUrl.Contains("/s_"))
+            baseUrl = baseUrl.Replace("/s_", "/b_");
+        else if (baseUrl.Contains("/m_"))
+            baseUrl = baseUrl.Replace("/m_", "/b_");
+
+        string fileExtension = Path.GetExtension(baseUrl); // .png, .jpg, .jpeg gibi
+        string baseWithoutExtension = baseUrl.Substring(0, baseUrl.LastIndexOf('-')); // Uzantıyı ve numarayı ayır
+
+        for (int i = 1; i <= maxImages; i++)
+        {
+            string updatedUrl = $"{baseWithoutExtension}-{i}{fileExtension}";
+            urls.Add(updatedUrl);
+        }
+
+        return urls;
+    }
+
+    static async Task DownloadAndSaveImage(HttpClient client, string imageUrl, string saveDirectory, string logFilePath)
+    {
+       
+
+        try
+        {
+            HttpResponseMessage response = await client.GetAsync(imageUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                byte[] imageBytes = await response.Content.ReadAsByteArrayAsync();
+                string fileName = Path.GetFileName(imageUrl);
+                string filePath = Path.Combine(saveDirectory, fileName);
+                await File.WriteAllBytesAsync(filePath, imageBytes);
+                Console.WriteLine($"Görsel indirildi: {filePath}");
+
+     
+            }
+            else
+            {
+                Console.WriteLine($"Görsel bulunamadı: {imageUrl}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Görsel indirme hatası: {ex.Message} - {imageUrl}");
+        }
+    }
+
+
+
+
 }
